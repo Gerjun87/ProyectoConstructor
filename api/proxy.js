e405716dff36a0c3c6687ef9bd122f4843a3e180
Gerardo <buck.ger@gmail.com>
2025-08-18 22:21:33 -0300

    Agrega carpeta apí y archivo proxy

---
+++ api/proxy.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const url = "https://script.google.com/macros/s/AKfycbzA074QM-7_pZKI-1XQxqcOpcZ1kRXZ_BrTgwI7n87ASwlmRZDWRJqAiG51_sVbMPU/exec";
+
+  try {
+    const response = await fetch(url, {
+      method: req.method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: req.method === "POST" ? JSON.stringify(req.body) : undefined,
+    });
+
+    const data = await response.json();
+
+    // 👇 acá liberamos CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
