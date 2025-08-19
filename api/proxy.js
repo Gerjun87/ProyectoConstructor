14d6786681fb0f47321320a07af8e260733c418b
Gerardo <buck.ger@gmail.com>
2025-08-18 22:34:46 -0300

    Actualiza proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  // 👇 si es preflight
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.status(200).end();
+    return;
+  }
+
   const url = "https://script.google.com/macros/s/AKfycbzA074QM-7_pZKI-1XQxqcOpcZ1kRXZ_BrTgwI7n87ASwlmRZDWRJqAiG51_sVbMPU/exec";
 
   try {
@@ -12,7 +21,6 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    // 👇 acá liberamos CORS
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
